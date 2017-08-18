0e3b90f473915626d19f920928aecfc0175d7c58
champ <champsamui8@gmail.com>
2017-08-18 15:25:56 +0700

    xxx

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Test xxx Facebook
+                   Test Facebook
                 </div>
 
                 <div class="links">
